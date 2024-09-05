4744605aee3956e718ab2b1737ed32566895abdb
Shaik Abdul Vahid <59871200+SAW-NAAZ@users.noreply.github.com>
2024-09-05 10:18:45 +0530

    Update demo.sh
    Dear Team There was a Bug I cleared and saved it.

--- demo.sh
+++ demo.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 echo "Good Morning"
-
 Welcome again to the Git Devops session 
+echo "Good Morning"
+Welcome again to the Git Devops session
+echo "Good Morning"
+Welcome again to the Git Devops session
+echo "Good Morning"
+Welcome again to the Git Devops session
+echo "Good Morning"
+Welcome again to the Git Devops session
+echo "Good Morning"
+Welcome again to the Git Devops session
